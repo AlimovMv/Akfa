326b577ed670c51eebedfd5ba1679f0b2bfead36
AlimovMv <alimov171098@gmail.com>
2020-06-24 21:28:00 +0600

    forpartners

--- Js/code.js
+++ Js/code.js
@@ -172,6 +172,26 @@ $(document).ready(function () {
             }
         }
     });
+
+    $("#carouselcertifications").owlCarousel({
+        loop: true, //Зацикливаем слайдер
+        margin: 50, //Отступ от элемента справа в 50px
+        nav: false, //Отключение навигации
+        autoplay: true, //Автозапуск слайдера
+        smartSpeed: 1000, //Время движения слайда
+        autoplayTimeout: 3000, //Время смены слайда
+        responsive: { //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 });
 
 function submitOnClick(buttonlink, user, password) {
